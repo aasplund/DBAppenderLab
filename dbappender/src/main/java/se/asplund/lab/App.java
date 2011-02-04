eabc1c7e28830233e37e4dd94e4d611dbe058c86
Anders Asplund <aasplund@hotmail.com>
2011-02-04 07:17:09 +0100

    Working db logging using jdbcappender and log4j

---
+++ dbappender/src/main/java/se/asplund/lab/App.java
@@ -0,0 +1,15 @@
+package se.asplund.lab;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
